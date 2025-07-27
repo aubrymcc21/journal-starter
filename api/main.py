8e42a528fb650d7ffca2ddd40bb35213269f5f9b
aubrymcc21 <aubrymcc21@gmail.com>
2025-07-27 19:14:56 -0400

    Updated Prometheus data metrics functionality.

--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest
+from fastapi import FastAPI, Request
+from fastapi.responses import Response
 from dotenv import load_dotenv
 from api.controllers import journal_router
 import logging
@@ -12,3 +14,16 @@ app = FastAPI()
 app.include_router(journal_router)
 
 logging.basicConfig(filename='myapp.log', level=logging.INFO)
+
+
+REQUEST_COUNT = Counter("http_requests_total", "Total HTTP Requests", ["method", "endpoint"])
+
+@app.middleware("http")
+async def count_requests(request: Request, call_next):
+    response = await call_next(request)
+    REQUEST_COUNT.labels(method=request.method, endpoint=request.url.path).inc()
+    return response
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
