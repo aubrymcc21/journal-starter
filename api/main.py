c2a157285dabe1e4384ecc3217a4053285f3d39b
aubrymcc21 <aubrymcc21@gmail.com>
2025-07-27 20:08:47 -0400

    Updated libraries for Prometheus.

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from prometheus_client import Counter, generate_latest
 from fastapi import FastAPI, Request
 from fastapi.responses import Response
+from prometheus_fastapi_instrumentator import Instrumentator
 from dotenv import load_dotenv
 from api.controllers import journal_router
 import logging
@@ -16,14 +17,4 @@ app.include_router(journal_router)
 logging.basicConfig(filename='myapp.log', level=logging.INFO)
 
 
-REQUEST_COUNT = Counter("http_requests_total", "Total HTTP Requests", ["method", "endpoint"])
-
-@app.middleware("http")
-async def count_requests(request: Request, call_next):
-    response = await call_next(request)
-    REQUEST_COUNT.labels(method=request.method, endpoint=request.url.path).inc()
-    return response
-
-@app.get("/metrics")
-def metrics():
-    return Response(generate_latest(), media_type="text/plain")
+Instrumentator().instrument(app).expose(app)
